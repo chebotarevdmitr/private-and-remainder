6f774d09ff3aa455b21e04b75ac599e550de222a
Dmitriy <127180093+chebotarevdmitr@users.noreply.github.com>
2024-06-30 17:07:12 +0300

    Добавьте файлы проекта.

---
+++ private and remainder.cpp
@@ -0,0 +1,15 @@
+﻿// private and remainder.cpp 
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Warmup: chastnoe i ostatok" << endl;
+	int a = 0, b = 0, chastnoe = 0, ostatok = 0;
+	cout << "\t input intenger A end B: ";
+	cin >> a >> b;
+	chastnoe = a / b;
+	ostatok = a % b;
+	cout << "chastnoe= " << chastnoe << ", ostatok= " << ostatok << endl;
+	return 0;
+
+}
