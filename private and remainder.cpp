d413288017b3081041ded0dbd698cb455cf65128
Dmitriy <127180093+chebotarevdmitr@users.noreply.github.com>
2024-06-30 17:41:17 +0300

    first correction

--- private and remainder.cpp
+++ private and remainder.cpp
@@ -3,13 +3,16 @@
 using namespace std;
 
 int main() {
-	cout << "Warmup: chastnoe i ostatok" << endl;
-	int a = 0, b = 0, chastnoe = 0, ostatok = 0;
-	cout << "\t input intenger A end B: ";
-	cin >> a >> b;
-	chastnoe = a / b;
-	ostatok = a % b;
-	cout << "chastnoe= " << chastnoe << ", ostatok= " << ostatok << endl;
+	cout << "Warmup:  private and remainder." << endl;   //chastnoe i ostatok
+
+	int  divisible = 0, divider = 0, privat = 0, remainder = 0;
+	cout << "\t input intenger divisible end divider: ";  //vvesti celoe delimoe i delitel
+	cin >> divisible >> divider;
+
+	privat     =    divisible  /  divider;
+	remainder  =    divisible  %  divider;
+	cout << " private= " << privat << ",remainder = " << remainder << endl;
+
 	return 0;
 
 }
