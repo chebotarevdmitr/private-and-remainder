b5196821fef493412fcaad353bd700f4f419dfb8
DIM <4ebotarov_dim@inbox.ru>
2024-06-30 17:58:08 +0300

    second correction

--- private and remainder.cpp
+++ private and remainder.cpp
@@ -1,17 +1,16 @@
 ﻿// private and remainder.cpp 
 #include<iostream>
-using namespace std;
 
 int main() {
-	cout << "Warmup:  private and remainder." << endl;   //chastnoe i ostatok
+	std::cout << "Warmup:  private and remainder." << std::endl;   //chastnoe i ostatok
 
 	int  divisible = 0, divider = 0, privat = 0, remainder = 0;
-	cout << "\t input intenger divisible end divider: ";  //vvesti celoe delimoe i delitel
-	cin >> divisible >> divider;
+	std::cout << "\t input intenger divisible end divider: ";  //vvesti celoe delimoe i delitel
+	std::cin >> divisible >> divider;
 
 	privat     =    divisible  /  divider;
 	remainder  =    divisible  %  divider;
-	cout << " private= " << privat << ",remainder = " << remainder << endl;
+	std::cout << " private= " << privat << ",remainder = " << remainder << std::endl;
 
 	return 0;
 
